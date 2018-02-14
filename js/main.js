70f486c39e33e68b2a0af8f53328fd80df3d508f
Becca Tew <rebeccatew@gmail.com>
2018-02-14 12:44:36 -0700

    rps add

--- js/main.js
+++ js/main.js
@@ -1,18 +1,32 @@
-function startGame(e) {
-  var choice = e.target.id
-  console.log(choice)
-}
+// function startGame(e) {
+  // var choice = e.target.id
+//   console.log(choice)
+// }
+console.log("Hello!")
 
-var choices = document.getElementsByClassName('choice')
-for (var i in choices) {
-  try {
-    choices[i].addEventListener('click', startGame)
-  } catch(err) {
-    //First load
-  }
-}
+var userChoice = document.getElementsById('rock')
+var userChoice = document.getElementsById('paper')
+var userChoice = document.getElementsById('scissors')
+//
+// var choices = document.getElementsByClassName('choice') //this is a JS array (from class)
+// for (var i in choices) {
+//     try {
+//       choices[i].addEventListener('click', startGame)
+// }     catch(err) {
+//
+// }
 
-var userChoice = prompt("Do you choose rock, paper or scissors?")
+// var userChoice =
+// for (var i in choices) {
+//   try {
+//     choices[i].addEventListener('click', startGame)
+//   } catch(err) {
+//     //First load
+//   }
+// }
+
+// var userChoice = prompt("Do you choose rock, paper or scissors?")
+console.log(userChoice)
 
 var compChoice = Math.random()
 
